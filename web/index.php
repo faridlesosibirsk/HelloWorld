8c2a2791929b3130cfc2c4976fc592bf19d15be9
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-26 22:21:24 +0700

    init

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-require_once dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php';
 
 $config = require_once dirname(__DIR__) . '/config/web.php';
 (new yii\web\Application($config))->run();
